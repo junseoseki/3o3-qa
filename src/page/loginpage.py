3f978bc61c0561b08c2248492d8b58e5ca9f1f1b
junseoseki <dhfkapap@naver.com>
2026-01-17 15:33:37 +0900

    수정

--- src/page/loginpage.py
+++ src/page/loginpage.py
@@ -11,12 +11,9 @@ class loginpage(basepage):
         super().__init__(page, timeout)
     
     def try_login(self):
-        try:
-            self._get_locator(login_locator.kakao_login_button).click()
-            self._get_locator(login_locator.id_input).fill(os.getenv("ID"))
-            self._get_locator(login_locator.password_input).fill(os.getenv("PASSWORD"))
-            self._get_locator(login_locator.login_button).click()
-            self._wait_for_load_state()
-            logging.info("로그인 성공")
-        except Exception as e:
-            logging.error(f"로그인 실패: {e}")
+        self._get_locator(login_locator.kakao_login_button).click()
+        self._get_locator(login_locator.id_input).fill(os.getenv("ID"))
+        self._get_locator(login_locator.password_input).fill(os.getenv("PASSWORD"))
+        self._get_locator(login_locator.login_button).click()
+        self._wait_for_load_state()
+        logging.info("로그인 성공")
