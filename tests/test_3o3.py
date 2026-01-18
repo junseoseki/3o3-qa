22445b98064732d085fefa334876c8f40fc92777
junseoseki <dhfkapap@naver.com>
2026-01-18 16:36:10 +0900

    시나리오 변경

--- tests/test_3o3.py
+++ tests/test_3o3.py
@@ -1,9 +1,9 @@
 from src.page.loginpage import loginpage
-from playwright.sync_api import expect
+from playwright.sync_api import Page, expect
 from src.util.locator import login_locator as L
-from src.util.locator import mainpage_locator as M
 
-def test_login_scenario(page):
+
+def test_login_scenario(page: Page):
     login = loginpage(page)
     login.try_login()
     expect(page.locator(L.id_input)).to_be_visible()
@@ -11,8 +11,8 @@ def test_login_scenario(page):
     expect(page.locator(L.login_button)).to_be_visible()
     expect(page.locator(L.qr_login_button)).to_be_visible()
     
-def test_login_negative(page):
+def test_login_negative(page: Page):
     login = loginpage(page)
     login.try_login_with_testid()
     login._wait_for_load_state()
-    expect(page.locator(M.check_already_form)).to_be_visible()
+    expect(page.get_by_text("카카오계정 혹은 비밀번호가 일치하지 않습니다. 입력한 내용을 다시 확인해 주세요")).to_be_visible()
